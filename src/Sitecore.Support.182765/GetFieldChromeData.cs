5017612c510419759e1bd2f57efc3c45026a3630
sc-AlexanderNg <alex@sitecore.net>
2017-09-07 15:44:32 +0800

    fix ExperienceEditor when template field's Title contains double-quote sign

---
+++ src/Sitecore.Support.182765/GetFieldChromeData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Sitecore.Data.Fields;
+using Sitecore.Pipelines.GetChromeData;
+
+namespace Sitecore.Support.Pipelines.GetChromeData
+{
+    public class GetFieldChromeData : Sitecore.Pipelines.GetChromeData.GetFieldChromeData
+    {
+        public override void Process(GetChromeDataArgs args)
+        {
+            base.Process(args);
+            /* ALEX20170907 Calling back the same IF checking and value assignment for DisplayName as base class
+               The fix is only to modify the quote replacement when assigning the following:
+               args.ChromeData.DisplayName = HttpUtility.HtmlEncode(argument.DisplayName);
+             */
+            if ("field".Equals(args.ChromeType, StringComparison.OrdinalIgnoreCase))
+            {
+                args.ChromeData.DisplayName = args.ChromeData.DisplayName.Replace("&quot;", "\"");
+            }
+        }
+    }
+}
+
